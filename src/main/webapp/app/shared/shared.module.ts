5c6d9d9004f9198727707ce2099963187dfe823d
718191431@qq.com <github1017@@@>
2018-03-05 22:15:47 +0800

    Initial application generated by JHipster-4.14.0

---
+++ src/main/webapp/app/shared/shared.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import {
+    DantiSharedLibsModule,
+    DantiSharedCommonModule,
+    CSRFService,
+    AuthServerProvider,
+    AccountService,
+    UserService,
+    StateStorageService,
+    LoginService,
+    LoginModalService,
+    JhiLoginModalComponent,
+    Principal,
+    JhiTrackerService,
+    HasAnyAuthorityDirective,
+} from './';
+
+@NgModule({
+    imports: [
+        DantiSharedLibsModule,
+        DantiSharedCommonModule
+    ],
+    declarations: [
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective
+    ],
+    providers: [
+        LoginService,
+        LoginModalService,
+        AccountService,
+        StateStorageService,
+        Principal,
+        CSRFService,
+        JhiTrackerService,
+        AuthServerProvider,
+        UserService,
+        DatePipe
+    ],
+    entryComponents: [JhiLoginModalComponent],
+    exports: [
+        DantiSharedCommonModule,
+        JhiLoginModalComponent,
+        HasAnyAuthorityDirective,
+        DatePipe
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+
+})
+export class DantiSharedModule {}
